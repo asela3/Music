13cbfade256ebcf5253b97eb65c6639f6914faa0
asela3 <aselajayathilake3@gmail.com>
2023-05-21 14:32:14 +0530

    change welcome route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,7 +4,7 @@ import { requireSignin } from "../middlewares/auth.js";
 
 const router = express.Router();
 
-router.get("/welcome", requireSignin, auth.welcome);
+router.get("/welcome", auth.welcome);
 router.post("/pre-register", auth.preRegister);
 router.post("/register", auth.register);
 router.post("/login", auth.login);
