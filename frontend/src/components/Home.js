468eb73d05a66c2d19105661d848f357fb09c644
asela3 <aselajayathilake3@gmail.com>
2023-05-26 23:58:04 +0530

    new release

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,18 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Player from "./Player";
 import SongList from "./SongList";
+import songs from "../data/songs.json";
 import SongDetail from "./SongDetail";
 import SongListHeader from "./SongListHeader";
 import NavBar from "./NavBar";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/auth";
-import { s3 } from "../Aws";
 
 const Home = () => {
   const navigate = useNavigate();
   let fromLS = localStorage.getItem("auth");
   const [auth, setAuth] = useAuth();
-  const [songs, setSongs] = useState([])
 
   useEffect(() => {
     if (!fromLS) {
@@ -22,27 +21,6 @@ const Home = () => {
       navigate('/')
     }
   }, [fromLS]);
-
-  useEffect(() => {
-    const fetchJsonFile = async () => {
-      try {
-        const params = {
-          Bucket: "ashimusic",
-          Key: "songs.json",
-        };
-
-        const response = await s3.getObject(params).promise();
-        const fileContent = response.Body.toString();
-        const jsonContent = JSON.parse(fileContent);
-
-        setSongs(jsonContent);
-      } catch (error) {
-        console.error("Error fetching JSON file:", error);
-      }
-    };
-
-    fetchJsonFile();
-  }, []);
 
   const genre = auth?.user?.genre;
   const mySongs = songs.filter((i) => i.genre === genre);
